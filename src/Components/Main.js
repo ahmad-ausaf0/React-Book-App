1ab000df84f0e597412ed2c85c25f670170de201
ahmad-ausaf0 <35466676+ahmad-ausaf0@users.noreply.github.com>
2022-04-15 21:47:23 +0530

    Initial commit

---
+++ src/Components/Main.js
@@ -0,0 +1,24 @@
+import react from "react";
+const Main = () => {
+  return (
+    <>
+      <div className="header">
+        <div className="row1">
+          <h1>
+            A room without books is like <br /> a body without soul.
+          </h1>
+        </div>
+        <div className="row2">
+          <h2>Find your Books</h2>
+          <div className="search">
+            <input type="text" placeholder="Enter your book name"></input>
+            <button>
+              <i class="fas fa-search"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Main;
